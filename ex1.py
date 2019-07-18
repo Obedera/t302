954d8e1ee0f0fa391173f9dfc55c8ec09697b384
Obede <obede.silva7@gmail.com>
2019-07-18 17:00:21 -0300

    ambiente virtual

---
+++ ex1.py
@@ -0,0 +1,19 @@
+import random
+lista = []
+i = 0
+
+while i<20:
+    lista.append(random.randrange(0, 100))
+    i+=1
+
+def verificar_maior(vetor):
+    valormax = vetor[0]
+    for l in vetor:
+        if l>valormax:
+            valormax = l
+    return valormax
+
+def maior_valor(vetor):
+    return max(vetor, key=int)
+
+print(f'Lista {lista} valormax {verificar_maior(lista)} valormax phyton{maior_valor(lista)}')gi
